7b0e15cd68e8fd07357306ef8ecb6896e49e40c1
clearly-outsane <1starwarrior516@gmail.com>
2022-09-30 16:54:25 +0530

    feat: setup project with husky commitlint and lint-staged

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  rules: {
+    'prettier/prettier': 'error',
+  },
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  bracketSpacing: false,
+  singleQuote: true,
+  trailingComma: 'all',
+};
